912e954373ecdb5e202cab61a5a08dce0b330cc4
Florin <83332450+flawreen@users.noreply.github.com>
2023-08-27 16:58:16 +0300

    added comments

--- automatizatorinatorul2000.py
+++ automatizatorinatorul2000.py
@@ -1,8 +1,10 @@
 table_name = input("TABLE = ")
 query = ""
-no_values = int(input("NO. OF VALUES = "))
-print("Int positions [enter if empty]: ")
+no_values = int(input("NO. OF VALUES = "))  # number of inserts
+
+print("Int positions [enter if empty]: ")  # Positions in the insert that don't need quotes
 int_positions = [int(x) for x in input().split()]
+
 for i in range(no_values):
 	row = input()
 	values = row.split(",")
